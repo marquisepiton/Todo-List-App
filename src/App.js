3d3e57cc232d1c86725f6c7ec554389ba7c41e86
Marquise Piton <pitonmarquise@gmail.com>
2021-03-23 13:31:38 -0400

    Completed DeleteTask function

--- src/App.js
+++ src/App.js
@@ -2,24 +2,70 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { render } from "@testing-library/react";
 import List from "./component/List";
+import React from "react";
 
 // Parent
-function App() {
-  let tasks = ["task one", "task two", "task three"];
-  let numberOfTask = tasks.length;
-
-  return (
+class App extends React.Component{
+  
+  constructor(){
+    super();
     
+    //let tasks = ["task one", "task two", "task three"];
+    this.numberOfTask = 0;
+    this.completedTasks = [];
+
+    this.DeleteTask = this.DeleteTask.bind(this)
+
+    this.state = {
+
+      tasks: [
+        {id:1, desc:"Something1", status:1, created_at:123456},
+        {id:2, desc:"Something2", status:1, created_at:123456},
+        {id:3, desc:"Something3", status:1, created_at:123456},
+      ]
+
+
+    
+ 
+     
+
+
+    };
+  }
+  render(){
+  return (
     <div className="App">
-      <List tasks={tasks} numberOfTask={numberOfTask} />
+      <List
+        tasks={this.state.tasks}
+        numberOfTask={this.numberOfTask}
+        CreateTask={this.CreateTask}
+        DeleteTask={this.DeleteTask}
+        MarkDoneTask={this.MarkDoneTask}
+      />
     </div>
   );
+  }
+  DeleteTask(id) {
+    let currentId = id;
+    let filteredTasks = this.state.tasks.filter(tasks => tasks.id !== currentId);   
+    this.setState({
+      tasks: filteredTasks
+    });
+    
+  }
+  CreateTask(value) {
+   
+  }
+   MarkDoneTask(props) {
+    
+  }
+  componentDidUpdate(){
 
-  function DeleteTask(props) {}
-  function CreateTask(props) {}
-  function MarkDoneTask(props) {}
+  }
 
-  
+
+
+
+
 }
-
 export default App;
--- src/component/List.js
+++ src/component/List.js
@@ -13,35 +13,41 @@ function List(props) {
             <div className="card">
               <ul className="list-group list-group-flush">
                 <li className="list-group-item">
-                  <input></input>
+                  <input placeholder="What needs to be done?"></input>
                 </li>
                 {/* List of task of here.  */}
-
                 {props.tasks.map((task, index) => {
+                  function clickDelete() {
+                    props.DeleteTask(task.id);
+                  }
+                  function clickCreate() {
+                    props.CreateTask();
+                  }
+                  // Turn this into a component
                   return (
                     <li key={index} className="list-group-item">
                       <input
+                        id='task'
                         className="form-check-input"
                         type="radio"
                         name="radioNoLabel"
                         id="radioNoLabel1"
                         value=""
                         aria-label="..."
+                        onClick={clickCreate} 
                       ></input>
-                      {task}
-                      <button>
-
-                     
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        className="bi bi-x"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                      </svg>
+                      {task.desc} 
+                      <button onClick={clickDelete}>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-x"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                        </svg>
                       </button>
                     </li>
                   );
@@ -68,9 +74,5 @@ function List(props) {
     </React.Fragment>
   );
 }
-
-function renderTask(props) {}
-
-//  <li className="list-group-item"></li>
 
 export default List;
